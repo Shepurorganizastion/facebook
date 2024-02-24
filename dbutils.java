e064952de856655f657ead77282574d15fd74ec6
sharath shepur <sharathshepur@gmail.com>
2024-02-24 15:37:09 +0530

    commited from dev

--- dbutils.java
+++ dbutils.java
@@ -1,3 +1,7 @@
+Now i am in development branch
+
+
+
 Version Control: Git enables several developers to collaborate on a single project at the same time without hindering one another's contributions.
 It meticulously records every modification to the project's codebase, identifying the author and the timestamp ofeach change. This functionality is vital for synchronizing team activities and addressing any arising disputes.
 Backup and Restore: Changes in Git are committed to the local repository, creating a complete project history. This history provides a backup of every change, and it's possible to revert to any previous project state.
