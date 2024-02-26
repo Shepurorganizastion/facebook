b3c722244cad2e69ce57d56e33cb679c30035649
sharath shepur <sharathshepur@gmail.com>
2024-02-26 15:38:35 +0530

    vivo updated

--- vivo.sh
+++ vivo.sh
@@ -1,1 +1,3 @@
+git stash temporarily shelves (or stashes) changes you have made to your working copy so you can work on something else, andthen come back and re-apply them later on.
+
 this is for updating a line from dev branch to master branch
